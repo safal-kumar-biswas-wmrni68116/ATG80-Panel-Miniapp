24a5d9722f999c0bf5b2fe0d41d95372dbf0bce2
Safal Kumar Biswas <wm.rni68116@gmail.com>
2026-07-20 12:12:38 +0600

    Render all DPs Simple Version

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,52 +1,32 @@
 import React from 'react';
 import { Text, View } from '@ray-js/ray';
 import { useDevInfo, useActions, useDpSchema, useProps } from "@ray-js/panel-sdk";
-import { NavBar, Button, Icon } from '@ray-js/smart-ui';
+import { NavBar, Button} from '@ray-js/smart-ui';
 import styles from './index.module.less';
+import { TySwitch } from "@ray-js/components-ty";
 
 export function Home() {
 
-  // Get all dpSchema
   const dpSchema = useDpSchema();
-  console.log("dpSchema", dpSchema);
-
-  // const dpSchemaList = Object.entries(dpSchema);
-  // console.log("dpSchemaList", dpSchemaList[0][1].name);
-
-
-  // Get all dpState
   const dpState = useProps((state) => state);
+  const actions = useActions();
+
+  console.log("dpSchema", dpSchema);
   console.log("dpState", dpState);
 
-  
-  // const dpStateList = Object.entries(dpState);
-  // console.log("dpStateList", dpStateList[0][1]);
-
-
-
-  // const actions = useActions();
 
   return (
     <>
 
     <NavBar leftText="Washing Machine" leftTextType="home" />
     
-    {/* Render all dpState by making this pair */}
-    {/* <View>
-      {dpStateList.map(([name, value]) => (
-        <View key={name}>
-        <Text>{name}: {value}</Text>
-        </View>
-      ))}
-    </View> */}
 
-
-    {/* another way to display dpState directly*/}
+    {/* Render all DPs*/}
     <View>
-      {Object.keys(dpSchema).map((dpCode) => {
+      {dpState && Object.keys(dpSchema).map((dpCode) => {
         return (
           <View key={dpCode}>
-            <Text>{dpCode}: {dpState[dpCode]}</Text>
+            <Text>{dpCode}: {dpState?.[dpCode as keyof typeof dpState]}</Text>
           </View>
         );
       })}
